c44e4b71a0783d78b9376f12795f63127eaad738
Vilmos Nebehaj <vilmos@elotl.co>
2017-12-10 19:31:04 -0800

    Mount pseudo-filesystems like proc in unit chroot

--- pkg/server/helper.go
+++ pkg/server/helper.go
@@ -9,6 +9,71 @@ import (
 	"github.com/golang/glog"
 )
 
+type Mount struct {
+	Source string
+	Target string
+	Fs     string
+	Flags  int
+	Data   string
+}
+
+var Mounts = []Mount{
+	{
+		Source: "proc",
+		Target: "/proc",
+		Fs:     "proc",
+		Flags:  syscall.MS_NOSUID | syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_RELATIME,
+	},
+	{
+		Source: "devtmpfs",
+		Target: "/dev",
+		Fs:     "devtmpfs",
+		Flags:  syscall.MS_NOSUID | syscall.MS_RELATIME,
+		Data:   "mode=755",
+	},
+	{
+		Source: "devpts",
+		Target: "/dev/pts",
+		Fs:     "devpts",
+		Flags:  syscall.MS_NOSUID | syscall.MS_NOEXEC | syscall.MS_RELATIME,
+		// This data is from Alpine, might differ on other distributions.
+		Data: "mode=620,ptmxmode=000",
+	},
+	{
+		Source: "sysfs",
+		Target: "/sys",
+		Fs:     "sysfs",
+		Flags:  syscall.MS_NOSUID | syscall.MS_NODEV | syscall.MS_NOEXEC | syscall.MS_RELATIME,
+	},
+}
+
+func unmountSpecial() {
+	// Unmount in reverse order, since /dev/pts is inside /dev.
+	for i := len(Mounts) - 1; i >= 0; i-- {
+		m := Mounts[i]
+		glog.Infof("Trying to Unmount() %s; this might fail", m.Target)
+		if err := syscall.Unmount(m.Target, syscall.MNT_DETACH); err != nil {
+			glog.Warningf("Unmount() %s: %v", m.Target, err)
+		}
+	}
+}
+
+func mountSpecial() error {
+	for _, m := range Mounts {
+		if err := os.MkdirAll(m.Target, 0700); err != nil {
+			glog.Errorf("MkdirAll() %s: %v", m.Target, err)
+			return err
+		}
+		glog.Infof("Mounting %s -> %s", m.Source, m.Target)
+		if err := syscall.Mount(m.Source, m.Target, m.Fs, uintptr(m.Flags), m.Data); err != nil {
+			glog.Errorf("Mount() %s -> %s: %v", m.Source, m.Target, err)
+			unmountSpecial()
+			return err
+		}
+	}
+	return nil
+}
+
 // Helper function to start a unit in a chroot.
 func StartUnit(rootfs string, command []string) error {
 	glog.Infof("Starting new unit %v under rootfs '%s'", command, rootfs)
@@ -19,7 +84,7 @@ func StartUnit(rootfs string, command []string) error {
 			return err
 		}
 		if err := os.MkdirAll(oldrootfs, 0700); err != nil {
-			glog.Errorf("MkdirAll() %s: %v", rootfs, err)
+			glog.Errorf("MkdirAll() %s: %v", oldrootfs, err)
 			return err
 		}
 		if err := syscall.PivotRoot(rootfs, oldrootfs); err != nil {
@@ -35,6 +100,10 @@ func StartUnit(rootfs string, command []string) error {
 			return err
 		}
 		os.Remove("/.oldrootfs")
+		if err := mountSpecial(); err != nil {
+			glog.Errorf("mountSpecial(): %v", rootfs, err)
+			return err
+		}
 	}
 
 	cmd := exec.Command(command[0], command[1:]...)
@@ -48,5 +117,9 @@ func StartUnit(rootfs string, command []string) error {
 	}
 	glog.Infof("Unit %v under rootfs '%s' running as pid %d", command, rootfs, cmd.Process.Pid)
 
-	return cmd.Wait()
+	err := cmd.Wait()
+	if rootfs != "" {
+		unmountSpecial()
+	}
+	return err
 }
