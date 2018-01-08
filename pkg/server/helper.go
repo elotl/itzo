1f44b814f09600d4959d983cb9c94842b470b5b5
Vilmos Nebehaj <vilmos@elotl.co>
2018-01-08 13:55:21 -0800

    Move utility functions to util.go

--- pkg/server/helper.go
+++ pkg/server/helper.go
@@ -1,10 +1,7 @@
 package server
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -30,26 +27,6 @@ const (
 )
 
 var logbuf = make(map[string]*LogBuffer)
-
-func copyFile(src, dst string) error {
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return err
-	}
-	return out.Close()
-}
 
 func runUnit(command, env []string, unitout, uniterr *os.File, policy RestartPolicy) (err error) {
 	backoff := 1 * time.Second
@@ -223,88 +200,6 @@ func StartUnit(rootfs string, command []string, policy RestartPolicy) error {
 	}
 
 	return err
-}
-
-func resizeVolume() error {
-	mounts, err := os.Open("/proc/mounts")
-	if err != nil {
-		glog.Errorf("opening /proc/mounts: %v", err)
-		return err
-	}
-	defer mounts.Close()
-	rootdev := ""
-	scanner := bufio.NewScanner(mounts)
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.Split(line, " ")
-		if len(parts) < 2 || parts[1] != "/" {
-			continue
-		} else {
-			rootdev = parts[0]
-			break
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		glog.Errorf("reading /proc/mounts: %v", err)
-		return err
-	}
-	if rootdev == "" {
-		err = fmt.Errorf("can't find device the root filesystem is mounted on")
-		glog.Error(err)
-		return err
-	}
-	for count := 0; count < 10; count++ {
-		// It might take a bit of time for Xen and/or the kernel to detect
-		// capacity changes on block devices. The output of resize2fs will
-		// contain if it did not need to do anything ("Nothing to do!") vs when
-		// it resized the device ("resizing required").
-		cmd := exec.Command("resize2fs", rootdev)
-		var outbuf bytes.Buffer
-		var errbuf bytes.Buffer
-		cmd.Stdout = io.MultiWriter(os.Stdout, &outbuf)
-		cmd.Stderr = io.MultiWriter(os.Stderr, &errbuf)
-		glog.Infof("trying to resize %s", rootdev)
-		if err := cmd.Start(); err != nil {
-			glog.Errorf("resize2fs %s: %v", rootdev, err)
-			return err
-		}
-		if err := cmd.Wait(); err != nil {
-			glog.Errorf("resize2fs %s: %v", rootdev, err)
-			return err
-		}
-		if strings.Contains(outbuf.String(), "resizing required") ||
-			strings.Contains(errbuf.String(), "resizing required") {
-			glog.Infof("%s has been successfully resized", rootdev)
-			return nil
-		}
-		time.Sleep(1 * time.Second)
-	}
-	glog.Errorf("resizing %s failed", rootdev)
-	return fmt.Errorf("no resizing performed; does %s have new capacity?",
-		rootdev)
-}
-
-func getUnitDir(rootdir, unit string) string {
-	return filepath.Join(rootdir, unit)
-}
-
-func getUnitRootfs(rootdir, unit string) string {
-	return filepath.Join(getUnitDir(rootdir, unit), "ROOTFS")
-}
-
-func isEmptyDir(name string) (bool, error) {
-	f, err := os.Open(name)
-	if err != nil && !os.IsExist(err) {
-		return true, nil
-	} else if err != nil {
-		return false, err
-	}
-	defer f.Close()
-	_, err = f.Readdirnames(1)
-	if err == io.EOF {
-		return true, nil
-	}
-	return false, err
 }
 
 // This is a bit tricky in Go, since we are not supposed to use fork().
