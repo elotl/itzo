cb7b9e2efea36459385f3e1e6538329d921a58ce
Vilmos Nebehaj <vilmos@elotl.co>
2018-01-12 10:47:14 -0800

    Move unit-specific logic to unit.go

--- pkg/server/helper.go
+++ pkg/server/helper.go
@@ -4,202 +4,22 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/golang/glog"
 )
 
-const (
-	MAX_BACKOFF_TIME = 5 * time.Minute
-)
-
-type RestartPolicy int
-
-const (
-	RESTART_POLICY_ALWAYS    RestartPolicy = iota
-	RESTART_POLICY_NEVER     RestartPolicy = iota
-	RESTART_POLICY_ONFAILURE RestartPolicy = iota
-)
-
 var logbuf = make(map[string]*LogBuffer)
-
-func runUnit(command, env []string, unitout, uniterr *os.File, policy RestartPolicy) (err error) {
-	backoff := 1 * time.Second
-	for {
-		start := time.Now()
-		cmd := exec.Command(command[0], command[1:]...)
-		cmd.Env = env
-		cmd.Stdout = unitout
-		cmd.Stderr = uniterr
-
-		err = cmd.Start()
-		if err != nil {
-			// Start() failed, it is either an error looking up the executable,
-			// or a resource allocation problem.
-			glog.Errorf("Start() %v: %v", command, err)
-			return err
-		}
-		glog.Infof("Command %v running as pid %d", command, cmd.Process.Pid)
-
-		err = cmd.Wait()
-		d := time.Since(start)
-		if err != nil {
-			foundRc := false
-			if exiterr, ok := err.(*exec.ExitError); ok {
-				if ws, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-					foundRc = true
-					glog.Infof("Command %v pid %d exited with %d after %.2fs",
-						command, cmd.Process.Pid, ws.ExitStatus(), d.Seconds())
-				}
-			}
-			if !foundRc {
-				glog.Infof("Command %v pid %d exited with %v after %.2fs",
-					command, cmd.Process.Pid, err, d.Seconds())
-			}
-		} else {
-			glog.Infof("Command %v pid %d exited with 0 after %.2fs",
-				command, cmd.Process.Pid, d.Seconds())
-		}
-
-		switch policy {
-		case RESTART_POLICY_NEVER:
-			return err
-		case RESTART_POLICY_ONFAILURE:
-			if err == nil {
-				return nil
-			}
-		case RESTART_POLICY_ALWAYS:
-			// Fallthrough.
-		}
-
-		if err != nil {
-			backoff *= 2
-			if backoff > MAX_BACKOFF_TIME {
-				backoff = MAX_BACKOFF_TIME
-			}
-		} else {
-			// Reset backoff.
-			backoff = 1 * time.Second
-		}
-		glog.Infof("Waiting for %v before starting %v again", backoff, command)
-		time.Sleep(backoff)
-	}
-}
-
-func RestartPolicyToString(policy RestartPolicy) string {
-	pstr := ""
-	switch policy {
-	case RESTART_POLICY_ALWAYS:
-		pstr = "always"
-	case RESTART_POLICY_NEVER:
-		pstr = "never"
-	case RESTART_POLICY_ONFAILURE:
-		pstr = "onfailure"
-	}
-	return pstr
-}
-
-func StringToRestartPolicy(pstr string) RestartPolicy {
-	policy := RESTART_POLICY_ALWAYS
-	switch strings.ToLower(pstr) {
-	case "always":
-		policy = RESTART_POLICY_ALWAYS
-	case "never":
-		policy = RESTART_POLICY_NEVER
-	case "onfailure":
-		policy = RESTART_POLICY_ONFAILURE
-	default:
-		glog.Warningf("Invalid restart policy %s; using default 'always'\n",
-			pstr)
-	}
-	return policy
-}
 
 // Helper function to start a unit in a chroot.
 func StartUnit(unitdir string, command []string, policy RestartPolicy) error {
+	glog.Infof("Starting new unit %v in %s", command, unitdir)
 	unit, err := NewUnitFromDir(unitdir)
 	if err != nil {
 		return err
 	}
-	rootfs := unit.GetRootfs()
-	if _, err := os.Stat(rootfs); os.IsNotExist(err) {
-		// No chroot package has been deployed for the unit.
-		rootfs = ""
-	}
-	glog.Infof("Starting new unit %v in %s", command, unitdir)
-
-	// Open log pipes _before_ chrooting, since the named pipes are outside of
-	// the rootfs.
-	lp := unit.LogPipe
-	helperout, err := lp.OpenWriter(PIPE_HELPER_OUT, true)
-	if err != nil {
-		lp.Remove()
-		return err
-	}
-	defer helperout.Close()
-	unitout, err := lp.OpenWriter(PIPE_UNIT_STDOUT, false)
-	if err != nil {
-		lp.Remove()
-		return err
-	}
-	defer unitout.Close()
-	uniterr, err := lp.OpenWriter(PIPE_UNIT_STDERR, false)
-	if err != nil {
-		lp.Remove()
-		return err
-	}
-	defer uniterr.Close()
-
-	if rootfs != "" {
-		rootfsEtcDir := filepath.Join(rootfs, "/etc")
-		if _, err := os.Stat(rootfsEtcDir); os.IsNotExist(err) {
-			if err := os.Mkdir(rootfsEtcDir, 0755); err != nil {
-				glog.Errorf("Could not make new rootfs/etc directory: %s", err)
-				return err
-			}
-		}
-		if err := copyFile("/etc/resolv.conf", filepath.Join(rootfs, "/etc/resolv.conf")); err != nil {
-			glog.Errorf("copyFile() resolv.conf to %s: %v", rootfs, err)
-			return err
-		}
-		oldrootfs := fmt.Sprintf("%s/.oldrootfs", rootfs)
-		if err := syscall.Mount(rootfs, rootfs, "", syscall.MS_BIND, ""); err != nil {
-			glog.Errorf("Mount() %s: %v", rootfs, err)
-			return err
-		}
-		if err := os.MkdirAll(oldrootfs, 0700); err != nil {
-			glog.Errorf("MkdirAll() %s: %v", oldrootfs, err)
-			return err
-		}
-		if err := syscall.PivotRoot(rootfs, oldrootfs); err != nil {
-			glog.Errorf("PivotRoot() %s %s: %v", rootfs, oldrootfs, err)
-			return err
-		}
-		if err := os.Chdir("/"); err != nil {
-			glog.Errorf("Chdir() /: %v", err)
-			return err
-		}
-		if err := syscall.Unmount("/.oldrootfs", syscall.MNT_DETACH); err != nil {
-			glog.Errorf("Unmount() %s: %v", oldrootfs, err)
-			return err
-		}
-		os.Remove("/.oldrootfs")
-		if err := mountSpecial(); err != nil {
-			glog.Errorf("mountSpecial(): %v", rootfs, err)
-			return err
-		}
-	}
-
-	err = runUnit(command, os.Environ(), unitout, uniterr, policy)
-
-	if rootfs != "" {
-		unmountSpecial()
-	}
-
-	return err
+	return unit.Run(command, os.Environ(), policy)
 }
 
 // This is a bit tricky in Go, since we are not supposed to use fork().
@@ -268,6 +88,8 @@ func startUnitHelper(rootdir, name string, args, appenv []string, policy Restart
 			glog.Errorf("Unit %v (helper pid %d) exited with error %v", args, pid, err)
 		}
 		lp.Remove()
+		unit.Close()
+		// XXX: use LogBuffer via Units, too.
 	}()
 	return pid, nil
 }
--- pkg/server/unit_test.go
+++ pkg/server/unit_test.go
@@ -1,16 +1,18 @@
 package server
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-
-//func (u *Unit) GetRootfs() string {
-//func (u *Unit) GetStatus() (UnitStatus, error) {
-//func (u *Unit) SetStatus(status UnitStatus) error {
 
 func TestNewUnit(t *testing.T) {
 	tmpdir, err := ioutil.TempDir("", "itzo-test")
@@ -65,5 +67,176 @@ func TestStatus(t *testing.T) {
 		ss, err := u.GetStatus()
 		assert.Nil(t, err)
 		assert.Equal(t, s, ss)
+	}
+}
+
+func TestStringToRestartPolicy(t *testing.T) {
+	policyStrs := []string{"always", "never", "onfailure"}
+	for _, pstr := range policyStrs {
+		assert.Equal(t, pstr, RestartPolicyToString(StringToRestartPolicy(pstr)))
+	}
+	assert.Equal(t, StringToRestartPolicy("foobar"), RESTART_POLICY_ALWAYS)
+}
+
+func TestRestartPolicyToString(t *testing.T) {
+	policies := []RestartPolicy{
+		RESTART_POLICY_ALWAYS,
+		RESTART_POLICY_NEVER,
+		RESTART_POLICY_ONFAILURE,
+	}
+	for _, p := range policies {
+		assert.Equal(t, p, StringToRestartPolicy(RestartPolicyToString(p)))
+	}
+}
+
+func TestUnitRestartPolicyAlways(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "itzo-test")
+	assert.Nil(t, err)
+	defer tmpfile.Close()
+	tmpdir, err := ioutil.TempDir("", "itzo-test")
+	assert.Nil(t, err)
+	defer os.RemoveAll(tmpdir)
+	unit, err := NewUnit(tmpdir, "myunit")
+	assert.Nil(t, err)
+	defer unit.Close()
+	ch := make(chan error)
+	go func() {
+		err = unit.runUnitLoop(
+			[]string{"sh", "-c", fmt.Sprintf("echo $$ > %s; exit 1", tmpfile.Name())},
+			[]string{}, nil, nil, RESTART_POLICY_ALWAYS)
+		ch <- err
+	}()
+	pid := 0
+	tries := 0
+	select {
+	case err = <-ch:
+		// Error, runUnitLoop() should not return.
+		assert.True(t, false)
+	case <-time.After(50 * time.Millisecond):
+		tries++
+		// Pid has not changed?
+		assert.True(t, tries < 20)
+		// Wait for pid to change (thus we know the application has been
+		// restarted).
+		contents, err := ioutil.ReadFile(tmpfile.Name())
+		assert.Nil(t, err)
+		newPid, err := strconv.Atoi(strings.Trim(string(contents), "\r\n"))
+		if err == nil {
+			if pid != 0 && newPid != pid {
+				break
+			}
+			pid = newPid
+		}
+	}
+}
+
+func err2rc(t *testing.T, err error) int {
+	exiterr, ok := err.(*exec.ExitError)
+	assert.True(t, ok)
+	ws, ok := exiterr.Sys().(syscall.WaitStatus)
+	assert.True(t, ok)
+	return ws.ExitStatus()
+}
+
+func TestUnitRestartPolicyNever(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "itzo-test")
+	assert.Nil(t, err)
+	defer tmpfile.Close()
+	tmpdir, err := ioutil.TempDir("", "itzo-test")
+	assert.Nil(t, err)
+	defer os.RemoveAll(tmpdir)
+	unit, err := NewUnit(tmpdir, "myunit")
+	assert.Nil(t, err)
+	defer unit.Close()
+	ch := make(chan error)
+	go func() {
+		err = unit.runUnitLoop(
+			[]string{"sh", "-c", fmt.Sprintf("echo $$ > %s; exit 1", tmpfile.Name())},
+			[]string{}, nil, nil, RESTART_POLICY_NEVER)
+		ch <- err
+	}()
+	select {
+	case err = <-ch:
+		assert.NotNil(t, err)
+	case <-time.After(10 * time.Second):
+		assert.True(t, false)
+	}
+	// Check return value.
+	assert.Equal(t, 1, err2rc(t, err))
+	contents, err := ioutil.ReadFile(tmpfile.Name())
+	assert.Nil(t, err)
+	pid, err := strconv.Atoi(strings.Trim(string(contents), "\r\n"))
+	assert.Nil(t, err)
+	assert.True(t, pid > 0)
+}
+
+func TestUnitRestartPolicyOnFailureHappy(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "itzo-test")
+	assert.Nil(t, err)
+	defer tmpfile.Close()
+	tmpdir, err := ioutil.TempDir("", "itzo-test")
+	assert.Nil(t, err)
+	defer os.RemoveAll(tmpdir)
+	unit, err := NewUnit(tmpdir, "myunit")
+	assert.Nil(t, err)
+	defer unit.Close()
+	ch := make(chan error)
+	go func() {
+		err = unit.runUnitLoop(
+			[]string{"sh", "-c", fmt.Sprintf("echo $$ > %s; exit 0", tmpfile.Name())},
+			[]string{}, nil, nil, RESTART_POLICY_ONFAILURE)
+		ch <- err
+	}()
+	select {
+	case err = <-ch:
+		assert.Nil(t, err)
+	case <-time.After(10 * time.Second):
+		assert.True(t, false)
+	}
+	contents, err := ioutil.ReadFile(tmpfile.Name())
+	assert.Nil(t, err)
+	pid, err := strconv.Atoi(strings.Trim(string(contents), "\r\n"))
+	assert.Nil(t, err)
+	assert.True(t, pid > 0)
+}
+
+func TestUnitRestartPolicyOnFailureSad(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "itzo-test")
+	assert.Nil(t, err)
+	defer tmpfile.Close()
+	tmpdir, err := ioutil.TempDir("", "itzo-test")
+	assert.Nil(t, err)
+	defer os.RemoveAll(tmpdir)
+	unit, err := NewUnit(tmpdir, "myunit")
+	assert.Nil(t, err)
+	defer unit.Close()
+	ch := make(chan error)
+	go func() {
+		err = unit.runUnitLoop(
+			[]string{"sh", "-c", fmt.Sprintf("echo $$ > %s; exit 1", tmpfile.Name())},
+			[]string{}, nil, nil, RESTART_POLICY_ONFAILURE)
+		ch <- err
+	}()
+	pid := 0
+	tries := 0
+	select {
+	case err = <-ch:
+		// Error, runUnitLoop() should not return.
+		assert.True(t, false)
+	case <-time.After(50 * time.Millisecond):
+		tries++
+		// Pid has not changed?
+		assert.True(t, tries < 20)
+		// Wait for pid to change (thus we know the application has been
+		// restarted).
+		contents, err := ioutil.ReadFile(tmpfile.Name())
+		assert.Nil(t, err)
+		newPid, err := strconv.Atoi(strings.Trim(string(contents), "\r\n"))
+		if err == nil {
+			if pid != 0 && newPid != pid {
+				break
+			}
+			pid = newPid
+		}
 	}
 }
