66b3abfd50a349a33929f2b447006f4f1b4c9005
Brendan Cox <bcox@elotl.co>
2018-10-25 16:29:25 -0700

    get rid of environments for itzo image, update itzo_download.sh script

--- scripts/elotl/configure.sh
+++ scripts/elotl/configure.sh
@@ -6,20 +6,6 @@ step() {
 	printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
 }
 
-ENVIRONMENT="dev"
-while [[ -n "$1" ]]; do
-    case "$1" in
-	"-e"|"--environment")
-	    shift
-	    ENVIRONMENT="$1"
-            shift
-            ;;
-        *)
-	    echo "Unknown argument $1"
-	    ;;
-    esac
-done
-
 step 'Set up timezone'
 setup-timezone -z US/Pacific
 
@@ -42,24 +28,6 @@ sed -Ei \
 step 'Create ld-linux-x86-64.so.2 link'
 mkdir -p /lib64
 ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
-
-
-if [ $ENVIRONMENT == "dev" ]; then
-    step 'Install ssh authorized keys'
-    mkdir -p /root/.ssh
-    chmod 0700 /root/.ssh
-    cat > /root/.ssh/authorized_keys <<-EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDPU7h8CaYA1VH/CwY3Ahw0s0wPbB/8t7A96GX/6qS2a8n79nGjywThZJP15L7NnTdTrdV59NEC1QvS0ym/JhlpokXwgMWURsYOAP5Y2lmK7wvAZ65bDn0iPiXXgyWPtEWQqCTV0U9HfZ81m+JMzfcED+L3w0iZAHSeRlupPZRtea3izx91A19RRn0NyVtmrwF4h3g537p+0O3DvaktxZddnwa3vPbY3CE6Eijiqsy9HOrx49YJS3SdBMvGNx91pynVLPWTCziBmYZCt8ioTGNvF8YWLVRf6VCj6M9zTG2NkCbXydAxpfRByTa+4yyKE44hmAehDM15pQGlmcg0O4HlepTqOvPZVyWAvkO3aD1xycrWSTKu68IgRzm9Ve064h3OUqVcWx1tybEAGyioC/H/vdJ4BGKH1wfQQvRbWrO8gCAr8LGS8JUIWWDPOCBtFobsyMo2opck9t8iM8lAiscueMNTJeRuIeK6692m0OsXL9+g8lHJkTD97VF963liCeRhaIG3kIaYXTyOhQdKbDQShT/r4yC8eMWDR/I6ab+2ir/qew46XwHJ98c/Ux0zII5v252D5Q/A4Wf6HJGOjAoMx4iQJ8Q5LYpLnIcX1WqznJQx1zPpaI9WpFe4ELK/mBAv53Emp3HjfacI74/RM6Zt/EHYcKV3Cr5VMaxgLaReaw== bcox@elotl.co
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/ywpZThNo+8665ftf8Se5hDfPewxsqvUiT2MifeBSHCHf0VSWKKDa9BG0o/gKVjEYsxrHBzR14SsPbhn2khP1jTKU1JPrSymqv2Zt5StGwadAWwXMn748zAu7mnIbioNn5pZ70K2eq/Q4LskQe0Sl9xpj92sPfLTEt3eAsPolIgvacy+qmlGmF7Zm+sFmmj+AvFyEFlhbsv8s92iwlGCNMXO2BkRTzYsTaLUVcf+xXbdxrxhTp15TnxzbMvBYTGSCnnCzK619zwDlY4MK33QWr3tEGVZalFXFX1NqRZbo1W8mfVHCG4bP/nMZZrROMTPmjl/KsMTC+4xRdzBlUO+7Fwd0qBEWyMe/90dtZajvXE20quLj7Dbbbx1UVuAXQXeiw5vTwPcwTBFqrbJ9dTyUqedQ2I/H8UeeQFkpTn3BC5yuccbpnVJg0yOBB4F4nUUdBSXCTTyz6I4EsBjhz12pyhGPljTgkcXJWmiv5bybD0EQQ+zZ4a077Ev9HTYo65xsCHr+Vh6KEy5TykV71lePgnfKpFTcsR5okuN9me8wRVpuytAZevnDwUhYblpBOl4GJo1yTZeQ+vPWKM3FJ7DvBY8w4TOdYsubcZwLcVIxEfMwTntohOOtATDQs8UlvLvhLPqtACDkpdHokb2YoDZFnewXwplReoe3hw7XlOb3cw== vilmos@x1
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCLoV/djnLOJt7gRprssm36m3kn2kwSYbjyh93A2/oHNcPM1rvONdeKQ2qAK/DbRVjGIIdcskDQWCAxTOsN4OEBkIOHsWlfE3TJ8f+IqGYyG1Ly+I66GIulWqZQfRb8H+4xKxVZ9C5g8eM58tYOAsToDFa/IX7RX2Ayu0BNc92+cUV8w92mL2c76MCwbKviipK7T6lDVn6IINxtJHVHwFZYonIiBYKn/30zGrL17sMbOVVd27lYPI/To61ui5PFSym7d/BOEkKRQKRgnHfpwm+VhXUuOGCv1FIeBZSs95JVhbIzynld8NBY3SjIGWi3m22HXR8yYlXRNZwB+BSrcWKL madhuri@elotl.co
-EOF
-    chmod 0600 /root/.ssh/authorized_keys
-    cat /root/.ssh/authorized_keys
-
-    step 'Enable ssh login for root'
-    echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-    cat /etc/ssh/sshd_config
-fi
 
 PW="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
 step "Setting root password to '$PW'"
@@ -90,42 +58,49 @@ EOF
 chmod 755 /etc/init.d/itzo
 cat /etc/init.d/itzo
 
-
 step 'Add itzo downloader-launcher'
-s3_bucket="itzo-download"
-if [ $ENVIRONMENT = "dev" ]; then
-    s3_bucket="itzo-dev-download"
-fi
 cat > /usr/local/bin/itzo_download.sh <<-EOF
 #!/bin/sh
 
 # upload file with cli:
 # aws s3 cp itzo s3://${s3_bucket}/ --acl public-read
 
-s3_url="http://${s3_bucket}.s3.amazonaws.com"
-app_dir=/usr/local/bin
-for app in cloud-init itzo; do
-    s3_path="\${s3_url}/\$app"
-    app_path="\${app_dir}/\$app"
-    rm -f \$app_path
-    while true; do
-        echo "\$(date) downloading \$app from S3" >> /var/log/itzo/itzo_download.log 2>&1
-        wget --timeout=3 \$s3_path -P \$app_dir && break >> /var/log/itzo/itzo_download.log 2>&1
-        sleep 1
-    done
-    chmod 755 \$app_path
+echo "-1000" > /proc/self/oom_score_adj
+itzo_dir=/usr/local/bin
+\${itzo_dir}/cloud-init --from-metadata-service >> /var/log/itzo/itzo.log 2>&1
+
+itzo_bucket="itzo-download"
+itzo_version="latest"
+if [[ -f /itzo_bucket ]]; then
+    itzo_bucket=\$(head -n 1 /itzo_bucket)
+fi
+if [[ -f /itzo_version ]]; then
+    itzo_version=\$(head -n 1 /itzo_version)
+fi
+s3_url="http://\${itzo_bucket}.s3.amazonaws.com"
+s3_path="\${s3_url}/itzo-\${itzo_version}"
+itzo_path="\${itzo_dir}/itzo"
+rm -f \$itzo_path
+while true; do
+    echo "\$(date) downloading itzo from S3" >> /var/log/itzo/itzo_download.log 2>&1
+    wget --timeout=3 \$s3_path -O \$itzo_path && break >> /var/log/itzo/itzo_download.log 2>&1
+    sleep 1
 done
 
-echo "-1000" > /proc/self/oom_score_adj
-\${app_dir}/cloud-init --from-metadata-service >> /var/log/itzo/itzo.log 2>&1
-\${app_dir}/itzo >> /var/log/itzo/itzo.log 2>&1
+chmod 755 \$itzo_path
+\${itzo_dir}/itzo >> /var/log/itzo/itzo.log 2>&1
 EOF
+
 chmod 755 /usr/local/bin/itzo_download.sh
 cat /usr/local/bin/itzo_download.sh
 
 step 'Add tosi'
 wget -O /usr/local/bin/tosi http://tosi-download.s3.amazonaws.com/tosi
 chmod 755 /usr/local/bin/tosi
+
+step 'Add cloud-init'
+wget -O /usr/local/bin/cloud-init http://itzo-download.s3.amazonaws.com/cloud-init
+chmod 755 /usr/local/bin/cloud-init
 
 step 'Add aws-ena module'
 wget http://itzo-packages.s3.amazonaws.com/aws-ena-module.tar.gz
