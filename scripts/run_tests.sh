7d57e2b325d72d5c18b7ee1c50770c749c6a456f
Brendan Cox <bcox@elotl.co>
2018-11-01 14:06:30 -0700

    testing out adding path in release section of run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,6 +4,7 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$SCRIPT_DIR/..
+
 cd $ROOT_DIR
 go test ./...
 
@@ -12,6 +13,7 @@ versiontag=""
 if [[ $TRAVIS_TAG =~ ^v[0-9].* ]] && [[ $TRAVIS_TAG == $TRAVIS_BRANCH ]]; then
     echo "How can you say you wanna sit there With all this funk going on?"
     echo "Get up. Time to release the beast!"
+    export PATH=$PATH:$HOME/.local/bin
     versiontag=$TRAVIS_BRANCH
     make itzo
     echo "Making an itzo release at $versiontag"
