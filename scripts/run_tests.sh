86dc1dabbdce67226eb855a2015ef5f066002f21
Brendan Cox <bcox@elotl.co>
2018-11-29 21:51:48 -0800

    more changes to run_tests in order to run on jenkins

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,17 +8,18 @@ ROOT_DIR=$SCRIPT_DIR/..
 cd $ROOT_DIR
 go test ./...
 
-# if our travis tag looks like a version and we're on a tagged branch
+CURRENT_TAG=$(git tag -l --points-at HEAD | head -n 1)
+
 versiontag=""
-if [[ $TRAVIS_TAG =~ ^v[0-9].* ]] && [[ $TRAVIS_TAG == $TRAVIS_BRANCH ]]; then
+if [[ $CURRENT_TAG =~ ^v[0-9].* ]]; then
     echo "How can you say you wanna sit there With all this funk going on?"
     echo "Get up. Time to release the beast!"
     export PATH=$PATH:$HOME/.local/bin
-    versiontag=$TRAVIS_BRANCH
+    versiontag=$CURRENT_TAG
     make itzo
     echo "Making an itzo release at $versiontag"
     release_file=itzo-$versiontag
-    release_bucket=itzo-download
+    release_bucket=itzo-bcox-download
     aws s3 cp itzo s3://$release_bucket/$release_file --acl public-read
     aws s3 cp itzo s3://$release_bucket/itzo-latest --acl public-read
 fi
