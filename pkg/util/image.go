2bcd66c237df6ea23e4c0fc19115cfd7a0a7d9ae
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-05-17 16:57:36 -0700

    Simplify image name parsing
    Image repos might have an arbitrary number of slashes, which breaks the
    current heuristic in certain cases.
    
    Instead, use a simple check to see if the first part of the image name
    is a hostname, for example quay.io/myimage refers to the image repo
    "myimage" on the registry server at quay.io.

--- pkg/util/image.go
+++ pkg/util/image.go
@@ -17,34 +17,23 @@ limitations under the License.
 package util
 
 import (
-	"fmt"
 	"strings"
 )
 
-// Most registry URLs don't have a leading scheme (e.g. http://).
-// This means that we can't use url.Parse to find the host name from
-// the image's path.  We'll use some heuristics to get the server and
-// image path.
+// Parse the image spec and extract the registry server and the repo name.
+// Example image specs:
+//   ECS: ACCOUNT.dkr.ecr.REGION.amazonaws.com/imagename:tag
+//   Docker Hub: imagename:tag or owner/imagename:tag
 func ParseImageSpec(image string) (string, string, error) {
 	server := ""
-	imageRepo := image
+	repo := image
 	var err error
 	parts := strings.Split(image, "/")
-	// ECS: ACCOUNT.dkr.ecr.REGION.amazonaws.com/imagename:tag
 	if len(parts) == 1 {
-		imageRepo = strings.Join([]string{"library", parts[0]}, "/")
-	} else if len(parts) >= 2 && strings.HasSuffix(parts[0], "amazonaws.com") {
+		repo = strings.Join([]string{"library", parts[0]}, "/")
+	} else if strings.Contains(parts[0], ".") {
 		server = parts[0]
-		imageRepo = strings.Join(parts[1:], "/")
-	} else if len(parts) == 2 {
-		//assumes docker registry: user/registry:tag
-		server = ""
-	} else if len(parts) >= 3 {
-		// everything else: server.tld/user/registry:tag
-		server = parts[0]
-		imageRepo = strings.Join(parts[1:], "/")
-	} else {
-		err = fmt.Errorf("Unknown image registry format")
+		repo = strings.Join(parts[1:], "/")
 	}
-	return server, imageRepo, err
+	return server, repo, err
 }
